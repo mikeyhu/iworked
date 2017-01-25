45be97681859d876b0b2faffadcd470944d33722
Michael Hughes <mikee.hughes@gmail.com>
2017-01-25 21:01:48 +0000

    tidy up bash script

--- iworked.sh
+++ iworked.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+cd "${0%/*}"
+LOCATION=$(pwd)
 cd ~
-java -jar bb_projects/iworked/target/uberjar/iworked-0.1.0-SNAPSHOT-standalone.jar "$@"
+java -jar ${LOCATION}/target/uberjar/iworked-0.1.0-SNAPSHOT-standalone.jar "$@"
